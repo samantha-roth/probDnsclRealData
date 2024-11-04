19b782a7427a01c7a925901f3dfc15a8f7dcb17f
Samantha M. Roth <41164073+samantha-roth@users.noreply.github.com>
2024-11-04 13:24:58 -0500

    Update runCodePlots.sh

--- runCodePlots.sh
+++ runCodePlots.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-#SBATCH --time=48:00:00
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --mem-per-cpu=10gb
-#SBATCH --partition=open
-
-source activate /storage/work/svr5482/probDnsclRealData
 
 cd /storage/work/svr5482/probDnsclRealData/code/plots
 Rscript plotAccuracyWholeRegion.R "/storage/work/svr5482/probDnsclRealData"
