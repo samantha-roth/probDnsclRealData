fccef71d202383876f2b3bc1728c170897dc777b
Samantha M. Roth <41164073+samantha-roth@users.noreply.github.com>
2024-11-19 16:54:51 -0500

    Create computeWSHfromWSE.R

---
+++ code/compareStorms/comparison/costgrow/computeWSHfromWSE.R
@@ -0,0 +1,32 @@
+#subtract high resolution elevations from high resolution WSEs
+
+
+rm(list=ls())
+
+library(terra)
+
+pt<- proc.time()
+
+setwd("C:/Users/svr5482/Documents/GitHub/probDnsclRealData")
+
+run5m<- rast("data/Outputs5m/Run_1.asc")
+dem5m<- rast("data/norristown_5m.asc")
+load("data/coords.5m.RData")
+elev5m<- c(as.matrix(extract(dem5m,coords.5m)))
+
+flood<- c("flood2014","flood2020","floodfuture")
+
+for(f in 1:length(flood)){
+  
+  run<- rast(paste0("data/",flood[f],"/CG.tif"))
+  WSE<- c(as.matrix(extract(run,coords.5m)))
+  WSH<- WSE- elev5m
+  WSH[which(WSH<0)]<-NA
+  WSHrast<- run5m
+  values(WSHrast)<- WSH
+  print(plot(WSHrast))
+  
+  writeRaster(WSHrast,file=paste0("data/",flood[f],"/CG_WSH.tif"),overwrite=TRUE)
+}
+
+
