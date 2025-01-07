84021682c4fc579ba3e4faa49d72bb90a34b213a
Samantha M. Roth <41164073+samantha-roth@users.noreply.github.com>
2025-01-07 14:28:17 -0500

    Update installPackages.R

--- code/installPackages.R
+++ code/installPackages.R
@@ -2,27 +2,15 @@
 
 rm(list=ls())
 
-install.packages("remotes")
-
-library(remotes)
-
-#for main analysis
-install_version("MASS","7.3-58.2")
-install_version("Matrix","1.6-1.1")
-
-#problem packages
-install_version("terra","1.7-3")
-install_version("akima","0.6-3.4")
-install_version("DescTools","0.99.54")
-install_version("fields","14.1")
-install_version("mvtnorm","1.1-3")
-install_version("spam","2.9-1")
-
-#for visualizations
-
-install_version("RColorBrewer","1.1-3")
-install_version("ggplot2","3.4.4")
-
-install_version("ggmap","3.0.1")
-install_version("rgdal","1.6-7")
-install_version("osmdata","0.2.5")
+install.packages("terra", type = "source")
+install.packages("akima")
+install.packages("DescTools")
+install.packages("MASS")
+install.packages("Matrix")
+install.packages("fields")
+install.packages("mvtnorm")
+install.packages("spam")
+install.packages("RColorBrewer")
+install.packages("ggplot2")
+install.packages("ggmap")
+install.packages("osmdata")
