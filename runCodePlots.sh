aeb7d7796f18d5472033a7bf78e21acaa7f87cf2
Samantha M. Roth <41164073+samantha-roth@users.noreply.github.com>
2025-01-09 12:07:47 -0500

    Update runCodePlots.sh

--- runCodePlots.sh
+++ runCodePlots.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 cd /storage/work/svr5482/probDnsclRealData/code/plots
-Rscript plotAccuracyWholeRegion.R "/storage/work/svr5482/probDnsclRealData"
+Rscript plotObsVSPreds.R "/storage/work/svr5482/probDnsclRealData"
 
 cd /storage/work/svr5482/probDnsclRealData/code/plots
 Rscript plotP.3mFlood_Dnscl.R "/storage/work/svr5482/probDnsclRealData"
 
 cd /storage/work/svr5482/probDnsclRealData/code/plots
 Rscript plot.3mFlood_Dnscl.R "/storage/work/svr5482/probDnsclRealData"
-
-cd /storage/work/svr5482/probDnsclRealData/code/plots
-Rscript plotP.3mFlood_Dnscl.R "/storage/work/svr5482/probDnsclRealData"
 
 cd /storage/work/svr5482/probDnsclRealData/code/plots
 Rscript plotCostGrow.3mFlood.R "/storage/work/svr5482/probDnsclRealData"
