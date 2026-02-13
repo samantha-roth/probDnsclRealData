08e1e649bd837aee9ba79dfd9ba8b3575e6721cb
Samantha M. Roth <41164073+samantha-roth@users.noreply.github.com>
2026-02-12 23:20:03 -0600

    ROC data and plots

--- code/comparison/berrocal_etal_2010/SLR_getROCdata.R
+++ code/comparison/berrocal_etal_2010/SLR_getROCdata.R
@@ -1,17 +1,16 @@
 #plot the ROC curve for Bayesian SLR
 
-
 #evaluate the performance of the Bayesian version of SLR for downscaling
 
 rm(list=ls())
 
 # dir<- commandArgs(trailingOnly=TRUE)
 # setwd(dir)
+# setwd("/Volumes/RothS/probDnsclRealData")
+# setwd("/dartfs/rc/lab/R/RothS/probDnsclRealData")
 setwd("/Users/f007f8t/Documents/GitHub/probDnsclRealData")
 
 library(terra)
-
-pt<- proc.time()
 
 #load region of interest coordinates
 load("data/boxAroundHWMs.5m.RData")
@@ -26,11 +25,12 @@ load("data/vals5minBdsAroundHWMs.RData")
 run10m<- rast("data/Outputs10m/Run_1.asc")
 run5m<- rast("data/Outputs5m/Run_1.asc")
 
-#load the thinned posterior samples from SLR fit via MCMC
-load("data/mcmc.output1_thinned")
+#load the posterior means from SLR fit via MCMC
+load("data/mcmc.means1")
 
 #define the vector holding thresholds to be used in the ROC curve
-threshold<- seq(0.01,1,by=0.01)
+threshold<- seq(0.001,1,by=0.001)
+
 
 ################################################################################
 #Next we use SLR to downscale WSH at 10m onto 5m grid
@@ -39,54 +39,36 @@ threshold<- seq(0.01,1,by=0.01)
 wsh.5m<- c(as.matrix(extract(run5m,coordsinBds.5m)))
 wsh.10m<- c(as.matrix(extract(run10m,coordsinBds.5m)))
 
-SLR_mcmc_pred_func<- function(step){
-  return(wsh.10m*beta1_thin[step] + beta0_thin[step])
+whichFlood<- ifelse(wsh.5m>0.3,1,0)
+
+mu<- wsh.10m*mcmc.means1["beta1"] + mcmc.means1["beta0"]
+
+pFlood<- rep(NA, length(wsh.10m))
+for(i in 1:length(wsh.10m)){
+  pFlood[i]<- 1-pnorm(0.3, mean= mu[i], sd = sqrt(mcmc.means1["sigma2"]))
 }
 
-#get downscaled values using the Bayesian SLR model
-downscale_vals<- matrix(NA, nrow=length(thin_inds), ncol= length(wsh.10m))
-for(i in 1:length(thin_inds)){
-  downscale_vals[i,]<- SLR_mcmc_pred_func(i)
-}
-
-downscale_vals[which(downscale_vals<0)]<- 0
-
-################################################################################
-#sensitivity and specificity
-
-#set the minimum threshold for flooding to be 0.3
-
-#what proportion of mcmc steps were flooded for each 5m resolution grid cell?
-flooded_func<- function(x) sum(ifelse(x>0.3,1,0))/length(x)
-prop_flooded_SLR<- apply(downscale_vals,2,flooded_func) 
-
-flooded_SLR<- matrix(NA, nrow=length(threshold),ncol=length(prop_flooded_SLR))
-for(t in 1:length(threshold)){
-  flooded_SLR[t,]<- ifelse(prop_flooded_SLR>threshold[t],1,0)  #is the proportion of steps that were flooded > threshold[t]?
-}
-
-flooded5m<- ifelse(wsh.5m>0.3,1,0)
-
-
-################################################################################
-################################################################################
-########################### get TPR and FPR ####################################
-################################################################################
-################################################################################
-
-#counting flooded as positive and not flooded as negative, need TPR = TP/P and FPR = 1- TN/N
-
-TPRbyThreshold<- rep(NA,length(threshold))
-TNRbyThreshold<- rep(NA,length(threshold))
+predFloodInds<- matrix(NA, nrow= length(threshold), ncol= length(wsh.5m))
+TPR<- rep(NA, length(threshold))
+TNR<- rep(NA, length(threshold))
 
 for(t in 1:length(threshold)){
-  TPRbyThreshold[t]<- length(which(flooded_SLR[t,]==1 & flooded5m==1))/length(which(flooded5m==1))
-  TNRbyThreshold[t]<- length(which(flooded_SLR[t,]==0 & flooded5m==0))/length(which(flooded5m==0))
+  predFloodInds[t,]<- ifelse(pFlood>=threshold[t],1,0)
+  TPR[t]<- length(which(predFloodInds[t,]==1 & whichFlood==1))/length(which(whichFlood==1))
+  TNR[t]<- length(which(predFloodInds[t,]==0 & whichFlood==0))/length(which(whichFlood==0))
+  
+  if(t%%100==0){
+    print(paste0("threshold=",threshold[t]))
+    print(paste0("TPR=", TPR[t]))
+    print(paste0("TNR=", TNR[t]))
+  }
 }
 
+FPR<- 1-TNR
+FNR<- 1-TPR
 
-FPRbyThreshold<- 1-TNRbyThreshold
+ROC_data_SB2010<- data.frame("TPR"= TPR,"FPR"= FPR,"TNR"=TNR,"FNR"=FNR)
 
-ROC_data_SB2010<- data.frame("TPR"= TPRbyThreshold,"FPR"= FPRbyThreshold)
 
 save(ROC_data_SB2010,file="data/ROC_data_SB2010")
+
